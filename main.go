75a7112633b8dca3f21087613ab351bbcd310119
iamnator <natverior1@gmail.com>
2021-10-31 14:12:59 +0100

    add dummy data

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main(){
+
+
 
 	router := gin.Default()
 	router.Static("/static", "./static")
@@ -15,6 +19,43 @@ func main(){
 	router.GET("/status", func(ctx *gin.Context) {
 		ctx.JSONP(200, "we are live!!")
 	})
+
+	router.GET("/get", func(ctx *gin.Context) {
+		str := `{
+  "status": true,
+  "code": 200,
+  "data": {
+      "current":  {
+          "speed": 34.89,
+          "dir": "56.00 degree north",
+          "created_at": "10:00 AM"
+      },
+      "log": [
+      {
+          "speed": 34.89,
+          "dir": "56.00 degree north",
+          "created_at": "10:00 AM"
+      },
+       {
+          "speed": 84.89,
+          "dir": "59.00 degree south",
+          "created_at": "9:45 AM"
+      },
+       {
+          "speed": 89.89,
+          "dir": "79.00 degree north",
+          "created_at": "8:00 AM"
+      }
+  ]
+  }
+}`
+		buf := strings.NewReader(str)
+		h := make(map[string]interface{})
+		json.NewDecoder(buf).Decode(&h)
+
+		ctx.JSONP(200, h)
+	})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
