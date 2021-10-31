7cd82b0f2b3d09e359870dd1f258d476d3c5d390
iamnator <natverior1@gmail.com>
2021-10-31 12:06:35 +0100

    ...

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main(){
 	router.Static("/static", "./static")
 	router.StaticFile("/favicon.ico", "./static/assets/img/favicon.png")
 	router.StaticFile("/", "./static/home.html")
-
+	router.GET("/status", func(ctx *gin.Context) {
+		ctx.JSONP(200, "we are live!!")
+	})
 	_ = http.ListenAndServe(":3000", router)
 }
